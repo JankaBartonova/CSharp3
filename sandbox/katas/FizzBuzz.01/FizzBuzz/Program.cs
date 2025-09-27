e620b5cee154fe4ffe19ccef2f37293adee85f9e
Jana Bartonova <janka.novakova.friends@gmail.com>
2025-09-27 11:24:01 +0200

    Implemented basic logic FizzBuzz app

---
+++ sandbox/katas/FizzBuzz.01/FizzBuzz/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Welcome to FizzBuzz!");
+Console.Write("Enter the upper bound: ");
+
+FizzBuzz fizzBuzz = new();
+int upperBound = int.Parse(Console.ReadLine());
+Console.WriteLine("You entered number: " + upperBound);
+Console.WriteLine("----------------------------------");
+
+fizzBuzz.CountTo(upperBound);
