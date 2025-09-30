44ddf9ffce709bc5e7901f6105dbec9e791ae095
Jana Bartonova <janka.novakova.friends@gmail.com>
2025-09-30 21:12:20 +0200

    Created basic web api server

---
+++ ToDoList/src/ToDoList.WebApi/Program.cs
@@ -0,0 +1,10 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/test", () => "Test");
+app.MapGet("/czechitas", () => "Czechitas");
+app.MapGet("/greetings/{name}", (string name) => $"Hello {name}!");
+app.MapGet("/add/{a:int}/{b:int}", (int a, int b) => $"Sum: {a} + {b} = {a + b}");
+
+app.Run();
