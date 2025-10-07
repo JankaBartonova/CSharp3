67c0acb5bea7b6a64f97a1eb0612615121da801e
Jana Bartonova <janka.novakova.friends@gmail.com>
2025-10-07 17:08:33 +0200

    Add new endpont in Czech Language

--- ToDoList/src/ToDoList.WebApi/Program.cs
+++ ToDoList/src/ToDoList.WebApi/Program.cs
@@ -6,5 +6,6 @@ app.MapGet("/test", () => "Test");
 app.MapGet("/czechitas", () => "Czechitas");
 app.MapGet("/greetings/{name}", (string name) => $"Hello {name}!");
 app.MapGet("/add/{a:int}/{b:int}", (int a, int b) => $"Sum: {a} + {b} = {a + b}");
+app.MapGet("/nazdarSvete", () => "Nazdar světe!");
 
 app.Run();
