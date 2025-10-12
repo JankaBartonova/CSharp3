e0719c38f295ac5f566bc48b213692eb1e9646eb
Jana Bartonova <janka.novakova.friends@gmail.com>
2025-10-12 21:09:32 +0200

    Copied code from lesson to ToDo project

--- ToDoList/src/ToDoList.WebApi/Program.cs
+++ ToDoList/src/ToDoList.WebApi/Program.cs
@@ -1,11 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+{
+    //Configure DI
+    builder.Services.AddControllers();
+}
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/test", () => "Test");
-app.MapGet("/czechitas", () => "Czechitas");
-app.MapGet("/greetings/{name}", (string name) => $"Hello {name}!");
-app.MapGet("/add/{a:int}/{b:int}", (int a, int b) => $"Sum: {a} + {b} = {a + b}");
-app.MapGet("/nazdarSvete", () => "Nazdar světe!");
+var app = builder.Build();
+{
+    // Configure Middleware (HTTP request pipeline)
+    app.MapControllers();
+}
 
 app.Run();
