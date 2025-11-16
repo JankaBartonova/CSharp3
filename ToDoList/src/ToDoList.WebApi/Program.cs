0f20101759c40070de8c9da1dd5c9b1806149512
Jana Bartonova <janka.novakova.friends@gmail.com>
2025-11-16 18:47:58 +0100

    Added Swagger

--- ToDoList/src/ToDoList.WebApi/Program.cs
+++ ToDoList/src/ToDoList.WebApi/Program.cs
@@ -8,12 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddControllers();
     builder.Services.AddDbContext<ToDoItemsContextBase, ToDoItemsContext>();
     builder.Services.AddScoped<IRepository<ToDoItem>, ToDoItemsRepository>();
+    builder.Services.AddSwaggerGen();
 }
 
 var app = builder.Build();
 {
     // Configure Middleware (HTTP request pipeline)
     app.MapControllers();
+    app.UseSwagger();
+    app.UseSwaggerUI(config => config.SwaggerEndpoint("v1/swagger.json", "ToDoList API V1"));
 }
 
 app.Run();
