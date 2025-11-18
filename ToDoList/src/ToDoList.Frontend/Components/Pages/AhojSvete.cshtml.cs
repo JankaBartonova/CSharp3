a852455423dd15db93ffb0a17910797e894f3a1a
Jana Bartonova <janka.novakova.friends@gmail.com>
2025-11-18 20:37:44 +0100

    Kod z lekce

---
+++ ToDoList/src/ToDoList.Frontend/Components/Pages/AhojSvete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ToDoList.Frontend.Components.Pages
+{
+    public class AhojSveteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
